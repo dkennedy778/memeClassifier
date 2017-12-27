da6d278bd30c99ca81aa312cffacc2868d24a887
dkennedy778 <derek.kennedy.13@cnu.edu>
2017-12-27 14:51:17 -0500

    Identifier hitting 90% accuracy on dataset of 1,000
    Hitting a "decompressed data too large error" somewhere during generation, need to figure out how to error handle these gosh darned things

--- memeIdentifier.py
+++ memeIdentifier.py
@@ -4,8 +4,9 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 import numpy as np
+import os
 
-#first we create a datagenerator that'll randomly modify our samples to diversify our limited sample of images, here we are doing the following
+#sample datagen which does the following
 #1.Randomly rotating the image within 40 degrees of the origin
 #2.randomly translating the images horizontally and vertically by at most 20%
 #3.scaling the models by a 1/255. factor to make it easier for our model to process, traditional RGB coefficents in the 0-255 range are too high for the model to process in a resonable amount of time
@@ -16,23 +17,24 @@ import numpy as np
 
 datagen = ImageDataGenerator(rotation_range=40,width_shift_range=.2,height_shift_range=.2,rescale=1./255,shear_range=.2,zoom_range=.2,horizontal_flip=True,fill_mode='nearest')
 
-#load the image
-img = load_img('dog_meme.jpg')
-x = img_to_array(img)
-x = x.reshape((1,) + x.shape)
+#inputDir = "D:\Users\Derek\PostGrad\memeClassifier\sampleMemesMain"
+# for image in os.listdir(inputDir):
+#     load the image
+    # img = load_img(image)
+    # x = img_to_array(img)
+    # x = x.reshape((1,) + x.shape)
 
-#randomly transform the image and output it to a save directory
-i = 0
-for batch in datagen.flow(x, batch_size=1,save_to_dir='sampleMemePermutations',save_prefix='dogMeme',save_format='jpeg'):
-    i+=1
-    #making 20 permutations
-    if i > 20:
-        break
-#Now 1 data point has become 20!
+    #randomly transform the image and output it to a save directory
+    # i = 0
+    # for batch in datagen.flow(x, batch_size=1,save_to_dir='sampleMemePermutations',save_prefix=image + 'perm',save_format='jpeg'):
+    #     i+=1
+        #making 20 permutations
+        # if i > 20:
+        #     break
+    #Now 1 data point has become 20!
 
-#Using 3 convolution layers here
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(3, 150, 150)))
+model.add(Conv2D(32, (3, 3), input_shape=(150, 150,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -54,49 +56,40 @@ model.add(Activation('sigmoid'))
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
-
 batch_size = 16
 
-generator = datagen.flow_from_directory(
-        'data/train',
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1./255)
+try:
+# this is a generator that will read pictures found in
+# subfolers of 'data/train', and indefinitely generate
+# batches of augmented image data
+    train_generator = train_datagen.flow_from_directory(
+       'D:/Users/Derek/PostGrad/memeClassifier/train',  # this is the target directory
+        target_size=(150, 150),  # all images will be resized to 150x150
+        batch_size=batch_size,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+# this is a similar generator, for validation data
+    validation_generator = test_datagen.flow_from_directory(
+        'D:/Users/Derek/PostGrad/memeClassifier/validation',
         target_size=(150, 150),
         batch_size=batch_size,
-        class_mode=None,  # this means our generator will only yield batches of data, no labels
-        shuffle=False)  # our data will be in order, so all first 1000 images will be cats, then 1000 dogs
-# the predict_generator method returns the output of a model, given
-# a generator that yields batches of numpy data
-bottleneck_features_train = model.predict_generator(generator, 2000)
-# save the output as a Numpy array
-np.save(open('bottleneck_features_train.npy', 'w'), bottleneck_features_train)
-
-generator = datagen.flow_from_directory(
-        'data/validation',
-        target_size=(150, 150),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
-bottleneck_features_validation = model.predict_generator(generator, 800)
-np.save(open('bottleneck_features_validation.npy', 'w'), bottleneck_features_validation)
-
-train_data = np.load(open('bottleneck_features_train.npy'))
-# the features were saved in order, so recreating the labels is easy
-train_labels = np.array([0] * 1000 + [1] * 1000)
-
-validation_data = np.load(open('bottleneck_features_validation.npy'))
-validation_labels = np.array([0] * 400 + [1] * 400)
-
-model = Sequential()
-model.add(Flatten(input_shape=train_data.shape[1:]))
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
-
-model.compile(optimizer='rmsprop',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-model.fit(train_data, train_labels,
-          epochs=50,
-          batch_size=batch_size,
-          validation_data=(validation_data, validation_labels))
-model.save_weights('bottleneck_fc_model.h5')
+        class_mode='binary')
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000 // batch_size,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800 // batch_size)
+    model.save_weights('first_try.h5')  # always save your weights after training or during training
+except Exception as e:
+    print ("error", e)
